efe54e0eb3bae6e1aad442f310c812205d69da47
Priyo Anggodho Arifin P. <anggabuduran@gmail.com>
2024-11-28 19:28:28 +0700

    Add files via upload

---
+++ SMK revit/PHP/1-11/lat02.php
@@ -0,0 +1,12 @@
+<?php
+
+    $tulisan = "Saya Belajar";
+    $tahun = '2024';
+    
+    echo $tulisan . " Pemrograman <br> di Tahun " . $tahun. '<br>';
+
+    var_dump($tulisan);
+    echo '<br>';
+    var_dump((int)$tahun + 2);
+
+?>
