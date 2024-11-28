5d450b6d16f0229fc742f71a0508115a2ecabb44
Priyo Anggodho Arifin P. <anggabuduran@gmail.com>
2024-11-28 19:33:25 +0700

    Delete SMK revit/PHP/1-11 directory

--- SMK revit/PHP/1-11/lat05.php
+++
@@ -1,9 +0,0 @@
-<?php  
-$nilai = 101;
-if ($nilai >=100 || $nilai <=0) {
-    echo "nilai benar";
-}
-else {
-    echo "nilai salah";
-}
-?>
--- SMK revit/PHP/1-11/nav.php
+++
@@ -1,7 +0,0 @@
-<nav>
-    <ul>
-        <li>kontak</li>
-        <li>sejarah</li>
-        <li>jurusan</li>
-    </ul>
-</nav>
