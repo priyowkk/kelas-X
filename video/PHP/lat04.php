054f18dad603f57ef3fba0436e734adff3678e75
Priyo Anggodho Arifin P. <anggabuduran@gmail.com>
2025-03-18 18:43:13 -0700

    Rapih

---
+++ video/PHP/lat04.php
@@ -0,0 +1,66 @@
+<?php 
+
+    $a = 12;
+    $b = 4;
+
+
+    echo $a + $b;
+
+    echo "<br>";
+
+    echo $a - $b;
+
+    echo "<br>";
+
+    echo $a * $b;
+    
+    echo "<br>";
+
+    echo $a / $b;
+
+    echo "<br>";
+
+    echo $a % $b;
+
+    echo "<br>";
+
+    echo $a > $b;
+
+    echo "<br>";
+
+    echo $a < $b;
+
+    echo "<br>";
+
+    echo $a == $b;
+
+    echo "<br>";
+
+    echo $a != $b;
+
+    echo "<br>";
+
+    echo $a >= $b;
+    
+    echo "<br>";
+
+    echo $a <= $b;
+
+    echo $a && $b;
+
+    echo "<br>";
+
+    echo $a || $b;
+
+    //op string
+
+    $kata = "sura";
+    $kota = "baya";
+
+    $hasil = $kata . $kota;
+    $hasil.=' KOTA PAHLWAN';
+    echo $hasil;
+    
+    
+
+?>
---
+++ video/PHP/lat05.php
@@ -0,0 +1,9 @@
+<?php  
+$nilai = 101;
+if ($nilai >=100 || $nilai <=0) {
+    echo "nilai benar";
+}
+else {
+    echo "nilai salah";
+}
+?>
